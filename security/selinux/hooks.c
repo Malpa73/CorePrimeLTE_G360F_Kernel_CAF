6f50eb0ceecc2479903635ad225b5fe491f47eb5
drago0nfly <greekgod97ak47@gmail.com>
2018-10-04 11:23:24 +0200

    selinux: fix a missing endif
    Signed-off-by: drago0nfly <greekgod97ak47@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -106,6 +106,7 @@ static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 int selinux_enforcing;
+#endif
 
 static int __init enforcing_setup(char *str)
 {
