7c6ba22de6cd7bf2f64b47df61e745bd9e673110
drago0nfly <greekgod97ak47@gmail.com>
2018-10-05 12:26:01 +0200

    msm: gfx3d_clk: overclock low binned and high binned devices up to 620 MHz
    Signed-off-by: drago0nfly <greekgod97ak47@gmail.com>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -575,6 +575,29 @@ static struct rcg_clk vfe0_clk_src = {
 	},
 };
 
+/* High Binned values */
+static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
+	F(  19200000,	      xo,   1,	  0,	0),
+	F(  50000000,  gpll0_aux,  16,	  0,	0),
+	F(  80000000,  gpll0_aux,  10,	  0,	0),
+	F( 100000000,  gpll0_aux,   8,	  0,	0),
+	F( 160000000,  gpll0_aux,   5,	  0,	0),
+	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
+	F( 200000000,  gpll0_aux,   4,	  0,	0),
+	F( 266670000,  gpll0_aux,   3,	  0,	0),
+	F( 294912000,	   gpll1,   3,	  0,	0),
+	F( 310000000,	   gpll2,   3,	  0,	0),
+	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 465000000,      gpll2,   2,	  0,	0),
+	F( 489473684,      gpll2, 1.9,	  0,	0),
+	F( 516666666,      gpll2, 1.8,	  0,	0),
+	F( 547058823,      gpll2, 1.7,	  0,	0),
+	F( 581250000,      gpll2, 1.6,	  0,	0),
+	F( 620000000,      gpll2, 1.5,	  0,	0),
+	F_END
+};
+
+/* Low Binned values */
 static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
@@ -587,6 +610,12 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 465000000,      gpll2,   2,	  0,	0),
+	F( 489473684,      gpll2, 1.9,	  0,	0),
+	F( 516666666,      gpll2, 1.8,	  0,	0),
+	F( 547058823,      gpll2, 1.7,	  0,	0),
+	F( 581250000,      gpll2, 1.6,	  0,	0),
+	F( 620000000,      gpll2, 1.5,	  0,	0),
 	F_END
 };
 
@@ -600,7 +629,7 @@ static struct rcg_clk gfx3d_clk_src = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
-			400000000),
+			620000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
@@ -2735,6 +2764,46 @@ static struct clk_lookup msm_clocks_lookup[] = {
 	CLK_LIST(gcc_crypto_axi_clk),
 	CLK_LIST(crypto_clk_src),
 };
+
+#define EFUSE_BASE	0x0005c004
+#define EFUSE_BASE1	0x0005c00c
+
+static void gcc_gfx3d_fmax(struct platform_device *pdev)
+{
+	void __iomem *base;
+
+	u32 pte_efuse, shift = 2, mask = 0x7;
+	int bin, version;
+
+	base = devm_ioremap(&pdev->dev, EFUSE_BASE, SZ_8);
+	if (!base) {
+		pr_err("unable to ioremap efuse base\n");
+		return;
+	}
+	pte_efuse = readl_relaxed(base);
+	devm_iounmap(&pdev->dev, base);
+	version = (pte_efuse >> 18) & 0x3;
+	if (!version)
+		return;
+
+	base = devm_ioremap(&pdev->dev, EFUSE_BASE1, SZ_8);
+	if (!base) {
+		pr_err("unable to ioremap efuse1 base\n");
+		return;
+	}
+	pte_efuse = readl_relaxed(base);
+	devm_iounmap(&pdev->dev, base);
+	bin = (pte_efuse >> shift) & mask;
+
+	if (bin != 2)
+		return;
+
+	pr_info("%s, Version: %d, bin: %d\n", __func__, version,
+					bin);
+
+	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 620000000;
+	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_465_clk;
+}
 
 static int msm_gcc_probe(struct platform_device *pdev)
 {
