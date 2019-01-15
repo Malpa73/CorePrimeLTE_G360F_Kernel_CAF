2b2de6d0a6c389a1f1c6b78fad619335f2012c3c
Malpa73 <kmktoczek@gmail.com>
2019-01-16 00:36:24 +0100

    fix and update config

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -1305,9 +1305,6 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 							pdata);
 		break;
 	case MDSS_EVENT_UNBLANK:
-		if (ctrl_pdata->refresh_clk_rate)
-			rc = mdss_dsi_clk_refresh(pdata);
-		mdss_dsi_get_hw_revision(ctrl_pdata);
 		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL); 
 		if (ctrl_pdata->on_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_unblank(pdata);
@@ -1331,8 +1328,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		ctrl_pdata->ctrl_state &= ~CTRL_STATE_MDP_ACTIVE;
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
-		if (!(pdata->panel_info.mipi.always_on))
-			rc = mdss_dsi_off(pdata, power_state);
+		rc = mdss_dsi_off(pdata, power_state);
 		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL); 
 		break;
 	case MDSS_EVENT_CONT_SPLASH_FINISH:
